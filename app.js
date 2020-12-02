528cce5d2c60874464a38fd7aeae5a92a97c5929
Brigitta Szikszai <sziszka0001@gmail.com>
2020-12-02 21:54:41 +0100

    add js functions

--- app.js
+++ app.js
@@ -17,3 +17,73 @@ function updateCounterAndPrice() {
     counter.innerText = selectedSeats.length;
     price.innerText = selectedSeats.length * ticketPrice;
 }
+
+const updateSelectedSeatsList = () => {
+    const selectedSeats = document.querySelectorAll(
+        ".seats .available-seat.selected"
+    );
+
+    const selectedSeatsIndexes = [...selectedSeats].map(seat =>
+        [...allSeats].indexOf(seat)
+    );
+
+    localStorage.setItem(
+        "selectedSeatsIndexes",
+        JSON.stringify(selectedSeatsIndexes)
+    );
+};
+
+const populateMovieList = () => {
+    movieList.movies.forEach(movie => {
+        const option = document.createElement("option");
+        option.setAttribute("value", movie.price);
+        option.innerText = `${movie.title} (${movie.price} ${movieList.currency})`;
+        movieSelected.appendChild(option);
+    });
+
+    ticketPrice = movieSelected.value;
+};
+
+const populateSeats = () => {
+    const seatsNumber = seatsDetails.rows * seatsDetails.columns;
+    for (let i = 0; i < seatsNumber; i++) {
+        const seat = document.createElement("div");
+        seat.classList.add("available-seat");
+        if (seatsDetails.occupied.includes(i)) {
+            seat.classList.add("occupied");
+        }
+        seatsContainer.appendChild(seat);
+    }
+
+    allSeats = document.querySelectorAll(".seats .available-seat");
+};
+
+const populateFromLocalStorage = () => {
+    const selectedSeatsIndexes = JSON.parse(
+        localStorage.getItem("selectedSeatsIndexes")
+    );
+
+    if (selectedSeatsIndexes !== null && selectedSeatsIndexes.length > 0) {
+        allSeats.forEach((seat, index) => {
+            if (selectedSeatsIndexes.indexOf(index) > -1) {
+                seat.classList.add("selected");
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+    if (selectedMovieIndex !== null) {
+        movieSelected.selectedIndex = selectedMovieIndex;
+        ticketPrice = movieSelected.value;
+    }
+
+    updateCounterAndPrice();
+};
+
+const populateUI = () => {
+    populateMovieList();
+    populateSeats();
+
+    populateFromLocalStorage();
+};
